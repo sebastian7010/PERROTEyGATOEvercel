8144a8947618fcdaf87cc763841187c5d42a0ada
sebastianssc <sebastianssc01@gmail.com>
2025-09-30 23:43:48 -0500

    subida de productos general corregidos comida

--- categories.js
+++ categories.js
@@ -1,28 +1,21 @@
 export const categories = [
-    // 🐶 Perros
     { id: 1, title: "Accesorios para Perros" },
     { id: 2, title: "Comida premium para Perros" },
     { id: 3, title: "Comida para Perros" },
     { id: 4, title: "Juguetes para Perros" },
 
-    // 🐱 Gatos
     { id: 5, title: "Accesorios para Gatos" },
     { id: 6, title: "Comida premium para Gatos" },
     { id: 7, title: "Comida para Gatos" },
     { id: 8, title: "Juguetes para Gatos" },
 
-    // 🐴 Caballos y suplementos
     { id: 9, title: "Suplementos y Productos para Caballos" },
 
-    // 🐄 Bovinos
     { id: 10, title: "Productos para Vacas y Bovinos" },
 
-    // 🐥 Aves
     { id: 11, title: "Productos para Aves y Pájaros" },
 
-    // 🐂 Terneros
     { id: 12, title: "Productos para Terneros" },
 
-    // 🐇 Conejos
     { id: 13, title: "Productos para Conejos" }
 ];
